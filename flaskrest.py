48b70424823e8d73f21965be7824b99710ab6d25
andblaeser <andblaeser@gmail.com>
2022-10-27 22:34:30 -0500

    initial commit

---
+++ flaskrest.py
@@ -0,0 +1,36 @@
+from flask import request
+from flask import Flask, url_for
+app = Flask(__name__)
+
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
+
+
+@app.route('/hello')
+def api_hello():
+    if 'name' in request.args:
+        return 'Hello ' + request.args['name']
+    else:
+        return 'Hello John Doe'
+
+
+@app.route('/echo', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+
+if __name__ == '__main__':
+    app.run()
